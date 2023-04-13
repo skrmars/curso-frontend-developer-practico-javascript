0737651ba7ce59a1e98a1903c5691d48345ab5ec
Oscar Marín <omarin@ethicsglobal.com>
2023-04-13 14:48:14 -0600

    se cambio la forma en que se crean los articulos con js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuIconMobile = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const iconShop = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuIconMobile.addEventListener("click",toggleMobileMenu);
@@ -35,4 +36,58 @@ function toggleCarritoAside() {
     }
 
     aside.classList.toggle("inactive");
+}
+
+const productList = [];
+productList.push({
+  name: 'bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Pc',
+  price: 650,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'cel',
+  price: 860,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+for(product of productList){
+  const productCard = document.createElement('div');
+  productCard.classList.add('product-card');
+
+  const productImg = document.createElement('img');
+  productImg.setAttribute('src', product.image);
+
+  const productInfo = document.createElement('div');
+  productInfo.classList.add('product-info');
+
+  const productDetail = document.createElement('div');
+
+  const productPrice = document.createElement('p');
+  productPrice.innerText = '$ ' + product.price;
+  const productName = document.createElement('p');
+  productName.innerText = '$ ' + product.name;
+  
+  productDetail.appendChild(productPrice);
+  productDetail.appendChild(productName);
+
+  const productFigure = document.createElement('figure');
+  
+  const productFigureImg = document.createElement('img');
+  productFigureImg.setAttribute('src','./icons/bt_add_to_cart.svg');
+
+  productFigure.appendChild(productFigureImg);
+  
+  productInfo.appendChild(productDetail);
+  productInfo.appendChild(productFigure);
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productInfo);
+
+  cardsContainer.appendChild(productCard); 
+
 }
