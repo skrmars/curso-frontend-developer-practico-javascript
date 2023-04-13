4e7f5f5c556e57f2cd4476f3f1a1e9e1d36c1df1
Oscar Marín <omarin@ethicsglobal.com>
2023-04-13 16:44:39 -0600

    se ajustaron los detalles del producto, clase 23

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuIconMobile = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const iconShop = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -11,7 +11,7 @@ menuIconMobile.addEventListener("click",toggleMobileMenu);
 iconShop.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
   if (!isAsideClosed) {
     aside.classList.add("inactive");
@@ -20,10 +20,10 @@ function toggleDesktopMenu() {
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
   if (!isAsideClosed) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
   mobileMenu.classList.toggle("inactive");  
 }
@@ -35,7 +35,7 @@ function toggleCarritoAside() {
       mobileMenu.classList.add("inactive");
     }
 
-    aside.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
